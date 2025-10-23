19cf24686fe201125d018300f4ce302f9120c27d
navbrar261 <navbrar261@gmail.com>
2025-10-23 12:59:28 -0400

    script.js is added
    it is added to add a message for console.

---
+++ script.js
@@ -0,0 +1,2 @@
+//I,Navdeep Kaur, 000922874, certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+console.log("Welcome to Navdeep Kaur's portfolio website!");
